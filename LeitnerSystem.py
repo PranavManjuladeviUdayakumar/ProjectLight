cb70b8f383a65f80ca4d502eea2b9a43003bebb2
PranavManjuladeviUdayakumar <PranavManjuladeviUdayakumar@gmail.com>
2023-08-15 13:07:18 +0530

    CLI ALL

--- LeitnerSystem.py
+++ LeitnerSystem.py
@@ -28,13 +28,19 @@ def insert(topics, category):
     con.commit()
 
 
-def update(pattern, category, streak=0):
+def update(category, pattern, streak=0):
     cur.execute(f"UPDATE Details SET Category = '{category}', Pattern='{pattern}', Streak={streak}, LastRun = '{today}'")
     con.commit()
 
 
+def remove(category):
+    cur.execute(f"DELETE FROM Details WHERE Category = '{category}'")
+    cur.execute(f"DELETE FROM Topics WHERE Category = '{category}'")
+    con.commit()
+
+
 def searchcategory():
-    return cur.execute("SELECT Category from Details").fetchall()
+    return cur.execute("SELECT * from Details").fetchall()
 
 
 def searchtopic(category):
@@ -42,17 +48,19 @@ def searchtopic(category):
 
 
 def higher(pattern):
-    if list(patterns.keys())[-1] == pattern:
+    lst = list(patterns.keys())
+    if lst[-1] == pattern:
         return pattern
     else:
-        return list(patterns.keys())[list(patterns.keys()).index(pattern) + 1]
+        return lst[lst.index(pattern) + 1]
     
 
 def lower(pattern):
-    if list(patterns.keys())[0] == pattern:
-        return pattern
+    lst = list(patterns.keys())
+    if lst[0] == pattern or lst[1] == pattern:
+        return lst[0]
     else:
-        return list(patterns.keys())[list(patterns.keys()).index(pattern) - 1]
+        return lst[lst.index(pattern) - 2]
     
 
 def overdue(lastrun, pattern):
@@ -60,6 +68,19 @@ def overdue(lastrun, pattern):
         return True
     else:
         return False
+
+
+def is_updateable(pattern, streak):
+    if patterns[pattern] <= streak:
+        return True
+    return False
+
+
+def proceed(message= "Do you wish to proceed: "):
+    ans = input(message).rstrip().lstrip()
+    if ans == 'y':
+        return True
+    return False
 
 
 def CLI():
@@ -73,6 +94,7 @@ Available Actions-
     4 - Exit
 
 Input Action Number: '''
+    
     while True:
 
         action = input(mainstr).strip()
@@ -80,25 +102,104 @@ Input Action Number: '''
         if action == '1':
             
             print("\nData Input Selected\n")
-            print("You will be asked to enter the category under which the topics fall under, and then the topics themselves. Enter the topics together, separated by a comma (,)\n")
+            print("You will be asked to enter the category under which the topics fall under, and then the topics themselves. \nEnter the topics together, separated by a comma (,)\n")
             category = input("Enter Category: ")
             topics = input("Enter all topics: ").split(',')
-            insert(topics, category)
+
+            print("Review topics to be added-")
+            for i in topics:
+                print(i.lstrip().rstrip(), category)
+            if proceed():
+                insert(topics, category)
+            else:
+                print("Action Aborted")
 
         elif action == '2':
-            print("Remove")
+            
+            print("\nData Removal Selected\n")
+            print("Enter the name of the category for whose data is to be deleted. NOTE: ALL records of a category will be removed.")
+            category = input("Category: ")
+
+            print("Review topics to be deleted-")
+            for i in searchtopic(input("Topic: ")):
+                print(i)
+            if proceed():
+                remove(category)
+            else:
+                print("Action Aborted")
+
         elif action == '3':
-            print("Practice")
+
+            print("\nPractice Mode\n")
+            print("You will be presented topics that are due for revision. \nDepending on your comfortability enter 'y' if you are sure of the topic, or 'n' if you aren't. \nThe more often a topic appears for you, the more revision you must do on the entire category. \nThe more comfortable you are with a category, the less it will appear for you.")
+            
+            if proceed():
+
+                print("Processing categories that need revision...")
+                queue = set()
+
+                for category, pattern, streak, lastrun in searchcategory():
+
+                    if overdue(lastrun, pattern):
+
+                        queue.add(category, pattern, streak)
+                
+                print("Categories processed!")
+                mistakequeue = set()
+
+                for i in queue:
+
+                    mistake = False
+                    print(f"Category to be revised: {i[0]}")
+
+                    for i in searchtopic(i[0]):
+                        print("Topic: ", i[0])
+                        if not proceed(message="Are you comfortable with this topic (y/n): "):
+                            mistake = True
+
+                    if mistake:
+                        mistakequeue.add(i)
+
+                print("All Categories Revised!")
+                print("Updating Frequency Charts...")
+
+                for i in (queue - mistakequeue):
+
+                    category, pattern, streak = i
+                    streak +=1
+
+                    if is_updateable(pattern, streak):
+
+                        pattern = higher(pattern)
+                        update(category, pattern)
+
+                    else:
+
+                        update(category, pattern, streak)
+
+                for i in mistakequeue:
+
+                    category, pattern, streak = i
+                    pattern = lower(pattern)
+
+                    update(category, pattern)
+
+
         elif action == '4':
+
+            con.close()
             print("Exiting...")
             sys.exit()
+
         else:
+
             print("Invalid Usage. Enter Numbers from 1 - 4 only.")
 
 
 def GUI():
-    print("GUI")
+    raise NotImplementedError
 
+#Main
 print(f'''
 ----------------------------------
                
@@ -119,14 +220,22 @@ while True:
     action = input("Enter Mode: ").strip()
 
     if action == '1':
+
         CLI()
+
     elif action == '2':
+
         GUI()
         break
+
     elif action == '3':
+
+        con.close()
         print('Exiting...')
         sys.exit()
+
     else:
+
         print("Invalid Usage. Enter Numbers from 1-3 only")
 
     
